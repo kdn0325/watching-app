f9881c8cd8dea96699cf5d0b59510840d31e7d30
kdn0325 <sexy-ehdsud@nate.com>
2022-03-09 02:17:39 +0900

    commit

--- src/component/movie-list/MovieList.jsx
+++ src/component/movie-list/MovieList.jsx
@@ -28,7 +28,7 @@ const MovieList = props => {
             setItems(response.results);
         }
         getList();
-    },[props.category,props.id,props.type]);
+    },[]);
     return (
         <div className="movie-list">
             <Swiper grabCursor={true} spaceBetween={10} slidesPerView={'auto'}>
