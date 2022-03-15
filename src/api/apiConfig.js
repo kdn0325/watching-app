449b761efb7f99808a77e143c64dc22e28f23bda
kdn0325 <sexy-ehdsud@nate.com>
2022-03-15 20:12:51 +0900

    update

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,7 @@
 const apiConfig = {
     baseUrl: "http://api.themoviedb.org/3/",
     apiKey:"df79b1492e6e2263c60a37bb046f355b",
+    language: 'ko',
     originalImage:(imgPath)=>`https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image:(imgPath)=>`https://image.tmdb.org/t/p/w500/${imgPath}`
 }
--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,6 +4,7 @@ import apiConfig from "./apiConfig";
 
 const axiosClient = axios.create({
     baseURL:apiConfig.baseUrl,
+    language:apiConfig.language,
     headers:{
         "Content-Type": "application/json"
     },
